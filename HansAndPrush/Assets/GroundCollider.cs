cf16b55dc3af6c2b844f5e63118e5dd5b129ed9b
viannone <VIannone@Vincents-iMac.local>
2017-02-02 20:22:02 -0800

    Starting to look like something
    ATS needs work still

--- HansAndPrush/Assets/GroundCollider.cs
+++ HansAndPrush/Assets/GroundCollider.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class GroundCollider : MonoBehaviour {
 	public CentralNervousSystem cns;
 
-	public void OnTriggerEnter2D(Collider2D coll){
+	public void OnTriggerStay2D(Collider2D coll){
 		cns.grounded = true;
 	}
 	public void OnTriggerExit2D(Collider2D coll){
